e8879d92b82a04ab72dc43f4ba687d0351ecd5d2
ZH-Dreamwear <1217243450@qq.com>
2018-12-25 20:03:23 +0800

    订单确认、详情页和订单列表

---
+++ src/service/order-service.js
@@ -0,0 +1,61 @@
+/**
+ * @Date:   2018-12-25T16:28:42+08:00
+ * @Last modified time: 2018-12-25T16:29:40+08:00
+ */
+
+
+
+'use strict';
+var _mm = require('util/mm.js');
+
+var _order = {
+    // 获取商品列表
+    getProductList : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/get_order_cart_product.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 提交订单
+    createOrder : function(orderInfo, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/create.do'),
+            data    : orderInfo,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取订单列表
+    getOrderList : function(listParam, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/list.do'),
+            data    : listParam,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取订单详情
+    getOrderDetail : function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/detail.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 取消订单
+    cancelOrder : function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/cancel.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _order;
